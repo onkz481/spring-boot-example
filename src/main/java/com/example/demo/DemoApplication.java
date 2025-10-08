3a6ce47bc89cfd582611478246123a389cbc97d7
kazuya ono <prvogo@gmail.com>
2025-10-09 06:22:19 +0900

    Init Commit.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	private String index() {
+		return "index";
+	}
+
+}
